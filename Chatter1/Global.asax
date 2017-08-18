134eb862f58094af58b35ab78ad7339d6170f1b0
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-08-18 10:28:26 -0400

    initial commit

---
+++ Chatter1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chatter1.MvcApplication" Language="C#" %>
---
+++ Chatter1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chatter1.Startup))]
+namespace Chatter1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
